c03acf98d993cb0c0133da9f3bdc074afb381992
Arisien <arisien@vivaldi.net>
2023-03-13 22:47:02 +0100

    Improvements/fixes to queue functions.

--- src/queues.cpp
+++ src/queues.cpp
@@ -6,34 +6,38 @@
 namespace Queues{
 
     template <typename T> bool Simple<T>::enqueue(T value){
-        Node *newNode = new Node;
-        newNode->value = value;
-        if(head == tail == nullptr){
-            head = tail = newNode;
-            newNode->next = nullptr;
-            return true;
-        }else{
-            tail->next = newNode;
-            newNode->next = nullptr;
-            tail = newNode;
-            return true
-        }
-        return false;
+        Node *node = new Node{value, nullptr};
+		if (!node) {
+		    return false;
+		}
+		if (!tail) {
+		    head = node;
+		    tail = node;
+		}
+		else {
+		    tail->next = node;
+			tail = node;
+		}
+		return true;
     }
 
     template <typename T> T Simple<T>::dequeue(){
-        T temp;
-        if(head == tail == nullptr) throw std::runtime_error("Dequeuing from empty Queue!");
-        if(head == tail){
-            temp = head->value;
-            delete head;
-            head = tail = nullptr;
-            return temp;
-        }
+        if(!head) {
+		    throw std::runtime_error("Dequeuing from empty Queue!");
+		}
+		if (head == tail) {
+		    tail = nullptr;
+		}
+		Node* next = head->next;
+		T value = head->value;
+		delete head;
+		head = next;
+		return value;
     }
 
     template <typename T> Cyclic<T>::Cyclic(size_t capacity){
-        this->cycle = new T[size];
+        this->cycle = new T[capacity];
+		// TODO: Implement.
         this->head = 0;
         this->tail = 0;
     }
