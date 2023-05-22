330efb67f09fced946de08c180738981ac966a91
martin25217 <martinrogosic@gmail.com>
2023-05-22 19:20:51 +0200

    Finished cyclic queue and started wirking on priority queue

--- includes/queues.hh
+++ includes/queues.hh
@@ -16,9 +16,14 @@ namespace Queues {
             Node* next;
             T value;
         };
-        Node* head;
-        Node* tail;
+        Node* head = nullptr;
+        Node* tail = nullptr;
         public:
+        /**
+         * @brief Destroy the Simple object
+         * 
+         */
+        ~Simple();
         /**
          * @brief Add an element to the queue. If the action was succesful return true, otherwise false.
          * 
@@ -27,7 +32,27 @@ namespace Queues {
          * @return false 
          */
         bool enqueue(T value);
+        /**
+         * @brief Remove and return an element from the queue.
+         * 
+         * @return T 
+         */
         T dequeue();
+
+        /**
+         * @brief Empty the queue.
+         * 
+         */
+        void clear();
+
+        /**
+         * @brief Search for an element in the list. If present return true otherwise return false. The action happens in O(n) time.
+         * 
+         * @param value 
+         * @return true 
+         * @return false 
+         */
+        bool has(T value);
     };
     /**
      * @brief Set size Queue which loops around itself.
@@ -42,16 +67,104 @@ namespace Queues {
             size_t capacity = 0;
             size_t length = 0;
         public:
+            /**
+             * @brief Construct a new Cyclic queue with a certain capacity
+             * 
+             * @param capacity 
+             */
             Cyclic(size_t capacity);
-            bool enqueue(T value);
+            /**
+             * @brief Add a element to the queue. If the action was succesful return true, otherwise false.
+             * 
+             * @param value 
+             * @return true 
+             * @return false 
+             */
+            /**
+             * @brief Destroy the Cyclic object
+             * 
+             */
+            ~Cyclic();
+
+            void enqueue(T value);
+            /**
+             * @brief Remove an element from the list. The function returns the removed element.
+             * 
+             * @return T 
+             */
             T dequeue();
+
+            /**
+            * @brief Search for an element in the list. If present return true otherwise return false. The action happens in O(n) time.
+            * 
+            * @param value 
+            * @return true 
+            * @return false 
+            */
+        bool has(T value);
+
+            /**
+             * @brief Empty the queue and set the head and tail at the start of it
+             * 
+             */
+            void reset();
     };
     /**
      * @brief Queue that allows priority enqueueing.
      * 
      */
     template <typename T>
-    class Priority;
+    class Priority{
+        private:
+        struct Node {
+            Node* next;
+            T value;
+        };
+        Node* head = nullptr;
+        Node* tail = nullptr;
+        bool flag;
+
+        /**
+         * @brief Construct a new Priority object.  If the flag passed is true the queue will sort itself in an ascending order and the 
+         * first elements to dequeue will be the ones with the highest weight. Otherwise the queue will be sorted in a descending order 
+         * and the elements with the lowest weight will be dequeued first.
+         * 
+         */
+        Priority(bool flag);
+        /**
+         * @brief Destroy the Priority object
+         * 
+         */
+        ~Priority();
+        /**
+         * @brief Enqueue an element into the queue. The element will be placed in the queue according to its weight.
+         * The function return true if succesful. Otherwise false.
+         * 
+         * @return true 
+         * @return false 
+         */
+        bool enqueue(T value);
+
+        /**
+         * @brief Dequeue the element with the highest or lowest priority depending on what the flag is set to.
+         * 
+         */
+        T dequeue();
+
+        /**
+         * @brief Return true if queue contains passed value. Otherwise false.
+         * 
+         * @return true 
+         * @return false 
+         */
+        bool has();
+
+        /**
+         * @brief Clear the queue of all its elements.
+         * 
+         */
+        void clear();
+    };
     /**
      * @brief Bidirectional queue
      * 
--- src/queues.cpp
+++ src/queues.cpp
@@ -2,6 +2,10 @@
 
 namespace Queues {
 
+	template <typename T> Simple<T>::~Simple(){
+		this->clear();
+	}
+
     template <typename T> bool Simple<T>::enqueue(T value){
         Node *node = new Node{value, nullptr};
 		if (!node) {
@@ -32,15 +36,81 @@ namespace Queues {
 		return value;
     }
 
+	template <typename T> bool Simple<T>::has(T value){
+		Node *iterator = head;
+
+        while (iterator){
+            if(iterator->value == value) return true;
+            iterator = iterator->next;
+        }
+        
+        return false
+	}
+
+	template <typename T> void Simple<T>::clear(){
+		Node *iterator = head;
+        Node *temp;
+
+        while (iterator){
+            temp = iterator;
+            iterator = iterator->next;
+            delete temp;
+        }
+
+        head = nullptr;
+        return;
+	}
+
     template <typename T> Cyclic<T>::Cyclic(size_t capacity){
         this->cycle = new T[capacity];
-		// TODO: Implement.
         this->head = 0;
         this->tail = 0;
     }
 
-    template <typename T> bool Cyclic<T>::enqueue(T value){
-        
+	template <typename T> Cyclic<T>::~Cyclic(){
+		delete this->cycle;
+	}
+
+    template <typename T> void Cyclic<T>::enqueue(T value){
+		this->cycle[(head++)%capacity] = value;
     }
+
+	template <typename T> T Cyclic<T>::dequeue(){
+		return this->cycle[(tail++)%capacity];
+	}
+
+	template <typename T> bool Cyclic<T>::has(T value){
+		for(int i = 0; i < length; i++){
+            if(this->array[i] == value) return true;
+        }
+        return false;
+	}
+
+	template <typename T> void Cyclic<T>::reset(){
+		for(int i = 0; i < this->capacity; i++){
+			this->cycle[i] = NULL;
+		}
+		head = 0;
+		tail = 0;
+		return;
+	}
+
+	template <typename T> Priority<T>::Priority(bool flag){
+		this->flag = flag;
+	}
+
+	template <typename T> Priority<T>::~Priority(){} 
+
+	template <typename T> bool Priority<T>::enqueue(T value){
+		//TODO: Finish this function
+		Node *newNode = new Node;
+		newNode->value = value;
+		Node *iterator = &head;
+
+		while(iterator)
+		
+	}
+
+
 
 }
